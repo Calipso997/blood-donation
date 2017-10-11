6647709bb034b224176a7f36b73c4a338a6695d6
Calipso997 <marekkamzol@tlen.pl>
2017-10-11 17:37:50 +0200

    secondDay

--- app/index.js
+++ app/index.js
@@ -1,4 +1,18 @@
-import './style.scss';
+import './style.css';
+import $ from 'jquery';
+import 'bootstrap';
 
 const port = 3000;
 const apiUri = `http://localhost:${port}`;
+
+
+    $(function () {
+        $('#datetimepicker5').datetimepicker({
+            defaultDate: "11/1/2013",
+            disabledDates: [
+                moment("12/25/2013"),
+                new Date(2013, 11 - 1, 21),
+                "11/22/2013 00:53"
+            ]
+        });
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -58,5 +58,15 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest']
         })
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            // In case you imported plugins individually, you must also require them here:
+            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+            ...
+        })
     ]
 };
