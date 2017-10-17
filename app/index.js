03569ccfddb3b5d42aba71705380ec18fb4d1412
Calipso997 <marekkamzol@tlen.pl>
2017-10-17 18:23:40 +0200

    17.10

--- app/index.js
+++ app/index.js
@@ -18,6 +18,28 @@ window.onload = () => {
             "11/22/2013 00:53"
         ]
     });
-
 };
 
+
+
+
+// pobranie danych z imputa
+
+
+// pobranie danych z radio
+let radioResult = document.getElementById("test").value;
+
+
+/*
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    date_input.datepicker(options);
+})
+*/
